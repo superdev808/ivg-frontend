456c1554b54933564fe2958f0c419b019d0a7027
kenethhu <kenethhu.dev@gmail.com>
2023-08-06 10:19:22 -0700

    set nodes selectable for interaction

--- src/components/flow/index.tsx
+++ src/components/flow/index.tsx
@@ -117,7 +117,7 @@ export const Flow = ({ mask }: any) => {
 						width: nodeWidth,
 						height: nodeHeight,
 						position,
-						selectable: false,
+						// selectable: false,
 						draggable: false,
 						
 						
