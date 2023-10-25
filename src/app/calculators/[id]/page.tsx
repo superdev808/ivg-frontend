4f722e41094a1b296c68bf63ec9d6a81f4201715
superdev808 <drose6307@gmail.com>
2023-10-24 19:51:38 -0700

    feat(calculator): Scan body implementation

--- src/app/calculators/[id]/page.tsx
+++ src/app/calculators/[id]/page.tsx
@@ -4,9 +4,9 @@ import { useGetCalculatorsQuery } from "@/redux/services/calculatorsApi";
 import { useRouter, useParams } from "next/navigation";
 import { useMemo, useState } from "react";
 import { Dropdown } from "primereact/dropdown";
-import { Divider } from "primereact/divider";
 import { Card } from "primereact/card";
 import { ProgressSpinner } from "primereact/progressspinner";
+import CalculatorContainer from "@/components/calculator";
 
 const tabItems = [
   {
@@ -41,6 +41,7 @@ export default function CalculatorPage() {
     data: calculatorOptions,
     error: calcError,
   } = useGetCalculatorsQuery(null);
+
   // const router = useRouter();
   const searchParams = useParams();
   const selectedType = useMemo(() => {
@@ -57,28 +58,31 @@ export default function CalculatorPage() {
       .sort((a, b) => a.value.localeCompare(b.value));
   }, [calculatorOptions, selectedType]);
 
-  const [scanBodyOption, setScanBodyOption] = useState(null);
+  const [scanBodyOption, setScanBodyOption] = useState<string | null>(null);
 
   return (
-    <div className="flex justify-content-center mt-6">
+    <div className="flex flex-column align-items-center justify-content-center mt-6">
       {isLoading ? (
         <ProgressSpinner />
       ) : (
-        <Card
-          className="w-12 md:w-5 flex px-4 py-2 border-round bg-white flex-column"
-          title={`What type of ${selectedType?.label} do you want?`}
-          subTitle={selectedType?.description}
-        >
-          <Dropdown
-            value={scanBodyOption}
-            onChange={(e) => setScanBodyOption(e.value)}
-            options={selectedOptions}
-            optionLabel="value"
-            optionValue="id"
-            placeholder={selectedType?.placeholder}
-            className="w-full"
-          />
-        </Card>
+        <>
+          <Card
+            className="w-12 md:w-5 flex px-4 py-2 border-round bg-white flex-column"
+            title={`What type of ${selectedType?.label} do you want?`}
+            subTitle={selectedType?.description}
+          >
+            <Dropdown
+              value={scanBodyOption}
+              onChange={(e) => setScanBodyOption(e.value)}
+              options={selectedOptions}
+              optionLabel="value"
+              optionValue="value"
+              placeholder={selectedType?.placeholder}
+              className="w-full"
+            />
+          </Card>
+          {scanBodyOption && <CalculatorContainer option={scanBodyOption.replaceAll(" ", "_")} />}
+        </>
       )}
     </div>
   );
