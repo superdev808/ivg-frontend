345e9e11328c385aaff17c4813a43030be078131
superdev808 <drose6307@gmail.com>
2023-11-29 06:35:01 -0800

    fix(footer): IV-53 - Update Footer Text

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 			<div className={'grid bg-tranparent m-0 z-2 h-5rem'}>
 
 				<div className="col-10 col-offset-1 md:col-4 md:col-offset-0 flex flex-column align-items-center">
-					<p className="text-center md:text-left">@ 2023 Ivory Guide, LLC and its subsidiaries. All rights reserved.</p>
+					<p className="text-center md:text-left">© 2023 Ivory Guide, LLC and its subsidiaries. All rights reserved.</p>
 				</div>
 				<div className="col-10 col-offset-1 md:col-4 md:col-offset-4 flex flex-column align-items-center">
 					<p className="text-center md:text-right">Report feedback/mistakes by emailing feedback@ivoryguide.com.</p>
