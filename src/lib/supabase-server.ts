770bc965168b50fad7f0621e9ec6eb5bee6a71e8
kenethhu <kenethhu.dev@gmail.com>
2023-11-30 01:10:49 -0800

    build fix attempt

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -1,10 +1,10 @@
-
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 // NOTE: `createServerComponentSupabaseClient` has been renamed to `createServerComponentClient` in version `0.7.0`
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 export default function createClient() {
+	const cookieStore = cookies();
 	return createServerComponentClient({
-		cookies,
+		cookies: () => cookieStore,
 	});
 }
