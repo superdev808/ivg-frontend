9749db882ebd9596ab00fa4fe1dd49718a0b3077
kenethhu <kenethhu.dev@gmail.com>
2023-11-30 21:16:43 -0800

    add devmode

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,33 @@
-'use client';
+// 'use client';
+
+// import { useEffect,useState } from "react";
+const devMode = process.env.NEXT_PUBLIC_DEV_MODE;
 
 const Footer = () => {
+	// const [dev, setDev] = useState('');
+	console.log('devMode',devMode)
+	
+	// useEffect(() => {
+	// 	console.log('devMode',process.env.DEV_MODE)
+	// 	if(devMode) {
+	// 		setDev('This is a development build.');
+	// 	}
+	// },[devMode]);
+
+
 	return (
 		<>
-			<div className={'grid bg-tranparent m-0 z-2 h-5rem'}>
-
-				<div className="col-10 col-offset-1 md:col-4 md:col-offset-0 flex flex-column align-items-center">
+			<div className={'grid bg-tranparent m-0 z-2 h-5rem justify-content-center'}>
+				<div className="col-10  md:col-4 flex flex-column align-items-center">
 					<p className="text-center md:text-left">© 2023 Ivory Guide, LLC and its subsidiaries. All rights reserved.</p>
+				</div >
+				<div className="col-10  md:col-4 flex flex-column text-center">
+
+					<p className="text-center">{devMode? <span className="text-red-700 border-1 border-red-700 border-round p-2 bg-red-100">This is a development build.</span> : ''}</p>
+
 				</div>
-				<div className="col-10 col-offset-1 md:col-4 md:col-offset-4 flex flex-column align-items-center">
+			
+				<div className="col-10  md:col-4 flex flex-column align-items-center">
 					<p className="text-center md:text-right">Report feedback/mistakes by emailing feedback@ivoryguide.com.</p>
 				</div>
 			</div>
