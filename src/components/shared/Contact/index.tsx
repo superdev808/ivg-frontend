b2e9ac4838cfca15df9b13414cbc91f8fb9f9c87
kenethhu <kenethhu.dev@gmail.com>
2023-12-21 14:10:08 -0800

    update layout

--- src/components/shared/Contact/index.tsx
+++ src/components/shared/Contact/index.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { Controller, useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { Dialog } from 'primereact/dialog';
+import FooterExtended from '@/components/footer/FooterExtended';
 
 export const ContactComponent = () => {
 	const [isSending, setIsSending] = useState(false);
@@ -268,62 +269,7 @@ export const ContactComponent = () => {
 						</p>
 					</div>
 				</div>
-				<div className="w-full">
-					<div
-						className={classNames(
-							'col-10 col-offset-1 pb-4 flex flex-column md:flex-row align-items-center justify-content-between border-bottom-1'
-						)}>
-						<div className="w-12 md:w-3 text-center">
-							<Image
-								src="/Ivory-Guide-Logo-Stack.svg"
-								alt="Ivory Guid Logo Stack"
-								width="200"
-								height={200 / 2.5}
-								className="relative"
-							/>
-						</div>
-						<div className={classNames('blur-shadow', 'flex justify-content-center my-3 w-3 w-12 md:w-3')}>
-							<Button
-								pt={{
-									icon: { style: { fontSize: '32px', color: 'var(--primary-dark-color)' } },
-								}}
-								rounded
-								text
-								icon="pi pi-facebook"
-								className="mx-2"
-								aria-label="Filter"
-							/>
-							<Button
-								pt={{
-									icon: { style: { fontSize: '32px', color: 'var(--primary-dark-color)' } },
-								}}
-								className="mx-2"
-								icon="pi pi-instagram"
-								rounded
-								text
-								aria-label="Filter"
-							/>
-							<Button
-								pt={{
-									icon: { style: { fontSize: '32px', color: 'var(--primary-dark-color)' } },
-								}}
-								className="mx-2"
-								icon="pi pi-linkedin"
-								rounded
-								text
-								aria-label="Filter"
-							/>
-						</div>
-						<div className="w-12 md:w-3 flex justify-content-center">
-							<Button
-								outlined
-								style={{ borderColor: 'var(--primary-dark-color)', color: 'var(--primary-dark-color)' }}
-								className={classNames('px-5 py-3 border-secondary')}>
-								Contact Us
-							</Button>
-						</div>
-					</div>
-				</div>
+				<FooterExtended />
 			</div>
 			<Dialog
 				showHeader={false}
