80a6a64cdbc0e4d90b64e3eb013940bc7b3e3a4a
alok.mali <alok.mali@galaxyweblinks.in>
2024-01-17 19:08:49 +0530

    wip - adjusted grid space

--- src/components/calculator/AllOnX/Item/Item.tsx
+++ src/components/calculator/AllOnX/Item/Item.tsx
@@ -67,10 +67,10 @@ const Item: React.FC<ItemDataParams> = ({
             isFirst && "border-top-1"
           } surface-border`}
         >
-          <div className="w-3 border-left-1 border-right-1 surface-border p-2 flex align-items-center">
+          <div className="w-4 border-left-1 border-right-1 surface-border p-2 flex align-items-center">
             {label}
           </div>
-          <div className="w-9">
+          <div className="w-8">
             {info.map((item: ItemInsights, i: number) => {
               const { itemName, itemNumber, link, quantity } = item;
               return (
