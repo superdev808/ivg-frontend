6b0aa8bea51e7f54cf1834a0e329393f78da9c28
himanshu.pal@galaxyweblinks.in <himanshu.pal@galaxyweblinks.in>
2024-01-30 15:40:15 +0530

    WIP

--- src/components/secure/calculator/AllOnX/PdfExport/PdfExport.tsx
+++ src/components/secure/calculator/AllOnX/PdfExport/PdfExport.tsx
@@ -4,18 +4,57 @@ import html2pdf from "html2pdf.js";
 
 const PDFExport: React.FC<any> = ({ pdfContent }) => {
   const contentRef = useRef(null);
-  const ExportAndSendPDF = () => {
+  const toastRef = useRef(null);
+
+  const ExportAndSendPDF = async (type: "download" | "export" = "download") => {
     const element = contentRef.current;
     if (element) {
-      html2pdf(element, {
-        margin: 10,
-        filename: "exported-document.pdf",
-        image: { type: "jpeg", quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-      }).then((pdf: any) => {
-        pdf.save();
-      });
+      try {
+        const options = {
+          margin: 10,
+          filename: "exported-document.pdf",
+          image: { type: "jpeg", quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+        };
+        // Create an html2pdf instance
+        if (type === "download") {
+          const pdfInstance = html2pdf(element, options);
+          await pdfInstance.output('blob');
+        } else if (type === "export") {
+          const blob = await html2pdf()
+            .set(options)
+            .from(element)
+            .outputPdf("blob", "my-invoice.pdf");
+
+          const formData = new FormData();
+          formData.append("attachment", blob, "exported-document.pdf");
+
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_APP_SERVER_URL}/sendAllOnXInfo`,
+            {
+              method: "POST",
+              body: formData,
+            }
+          );
+
+          if (!response.ok && toastRef.current) {
+            response.json().then((res: any) => {
+              (toastRef.current as any).show({
+                severity: "error",
+                summary: res.status,
+                detail: res.message,
+                life: 5000,
+              });
+            });
+            return;
+          }
+        }
+
+        
+      } catch (error) {
+        console.error("Error exporting to PDF or sending email:", error);
+      }
     }
   };
 
@@ -40,7 +79,7 @@ const PDFExport: React.FC<any> = ({ pdfContent }) => {
             cursor: "pointer",
             border: "none",
           }}
-          onClick={ExportAndSendPDF}
+          onClick={() => ExportAndSendPDF("export")}
         >
           Download PDF
         </button>
