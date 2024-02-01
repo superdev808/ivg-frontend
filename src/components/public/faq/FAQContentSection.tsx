7b2b2d9a78c87a42201e517d6647add5932931cf
kenethhu <kenethhu.dev@gmail.com>
2024-01-31 17:21:52 -0800

    style update

--- src/components/public/faq/FAQContentSection.tsx
+++ src/components/public/faq/FAQContentSection.tsx
@@ -22,18 +22,13 @@ export const FAQContentSection = () => {
 			content: (
 				<>
 					<div className="flex flex-column">
-						
-							<span className="col font-bold">support@ivoryguide.com:</span>
-							<span className="col">
-								For any customer support questions or incorrect information listed on site; for any additional information or questions you may have.
-							</span>
-					
-							<span className="col font-bold">feedback@ivoryguide.com:</span>
-							<span className="col">
-								For any feedback on the website or existing calculators
-							</span>
-						
-				
+						<span className="col font-bold">support@ivoryguide.com:</span>
+						<span className="col">
+							For any customer support questions or incorrect information listed on site; for any additional information or questions you may have.
+						</span>
+
+						<span className="col font-bold">feedback@ivoryguide.com:</span>
+						<span className="col">For any feedback on the website or existing calculators</span>
 					</div>
 				</>
 			),
@@ -61,40 +56,33 @@ export const FAQContentSection = () => {
 			header: 'How can I reset my password?',
 			content: (
 				<>
-				<p>
-				Go to the login page and select Forgot your password? Follow the prompt and enter the email that is associated with your account. You will receive an email that will redirect you to reset your password. 
-				</p>
-				<p>
-				{`You can also reset your password from your account settings. Go to Settings > Password & Security. CONT. 
+					<p>
+						Go to the login page and select Forgot your password? Follow the prompt and enter the email that is associated with your account. You will
+						receive an email that will redirect you to reset your password.
+					</p>
+					<p>
+						{`You can also reset your password from your account settings. Go to Settings > Password & Security. CONT. 
 `}
-				</p>
-				 </>
+					</p>
+				</>
 			),
 		},
 		{
 			header: 'How do I add my business’s information for the export feature?',
-			content: (
-				<>Will need to add an explanation when the section is built out.</>
-			),
+			content: <>Will need to add an explanation when the section is built out.</>,
 		},
 		{
 			header: 'Can I change my account’s email address?',
-			content: (
-				<>{`Yes! Go to Settings > Profile. There you will see a email… CONT.`}  </>
-			),
+			content: <>{`Yes! Go to Settings > Profile. There you will see a email… CONT.`} </>,
 		},
 		{
 			header: 'Can I export my Calculator Results? ',
-			content: (
-				<>Instructions</>
-			),
+			content: <>Instructions</>,
 		},
 		,
 		{
 			header: 'How do I access my saved Calculators?',
-			content: (
-				<>Instructions</>
-			),
+			content: <>Instructions</>,
 		},
 	];
 
@@ -107,13 +95,13 @@ export const FAQContentSection = () => {
 			<AccordionTab
 				pt={{
 					root: { className: cx('shadow-none') },
-					header: { className: cx('border-2 border-round-2xl' ) },
-					headeraction: { className: cx(' border-round-2xl',{'bg-primary': activeIndex === index + 1}) }, // eslint-disable-line
+					header: { className: cx('border-2 border-round-2xl') },
+					headeraction: { className: cx(' border-round-2xl', { 'bg-primary': activeIndex === index + 1 }) }, // eslint-disable-line
 					headertitle: { className: cx('w-full pr-1') },
-					content: { className: cx('p-0') },
+					content: { className: cx('p-0 bg-transparent') },
 				}}
-				header={<div className="font-bold text-sm ">{questionItems[index]?.header}</div>}>
-				<p className="m-0 py-3 text-sm">{questionItems[index]?.content}</p>
+				header={<div className="font-bold text-sm md:text-base">{questionItems[index]?.header}</div>}>
+				<p className="m-0 px-3 py-3 text-sm md:text-base	">{questionItems[index]?.content}</p>
 			</AccordionTab>
 		</Accordion>
 	);
@@ -131,19 +119,20 @@ export const FAQContentSection = () => {
 
 	return (
 		<>
-			<div className={cx('bg-secondary', 'h-5rem')}></div>
-
+			<div className={cx('bg-secondary', 'h-5rem ')}></div>
+			
 			<div className={cx('section-header')}>
-				<span className="text-2xl md:text-5xl font-bold">Frequently Asked Questions</span>
-				<span className="pt-4">{`We are here to help you. If you don't see your question here, please contact us.`}</span>
+				<span className="text-2xl md:text-5xl font-bold z-1">Frequently Asked Questions</span>
+				<span className="pt-4 z-1">{`We are here to help you. If you don't see your question here, please contact us.`}</span>
 			</div>
-
-			<div className="grid m-0 p-0 mx-4 md:mx-6 my-4 md:my-8">
-				{questionItems.map((item, index) => {
-					return renderAccordian(index);
-				})}
+			<div className="flex justify-content-center relative">
+			<div className={cx('background-radial-gradient')} ></div>
+				<div className="grid m-0 p-0 mx-2 md:w-8 my-4 md:my-8">
+					{questionItems.map((item, index) => {
+						return renderAccordian(index);
+					})}
+				</div>
 			</div>
-	
 
 			<CTASection
 				text={text}
