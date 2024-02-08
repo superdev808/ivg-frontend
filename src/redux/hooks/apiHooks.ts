d13a695954d5a06f242b62d7f027979f4d774b7a
Wayne Kuo <waynekuo1123@gmail.com>
2024-02-07 19:19:44 -0800

    update individual calculator output screen

--- src/redux/hooks/apiHooks.ts
+++ src/redux/hooks/apiHooks.ts
@@ -1,19 +1,18 @@
-import { userApiSlice } from '../slices/api/userApiSlice'
+import { userApiSlice } from "../slices/api/userApiSlice";
 
 // RTK Query Hooks
 
-
 export const {
-
-    usePostCheckEmailMutation,
-    usePostRegisterUserMutation,
-    useGetVerifyUserQuery,
-    usePostRequestPasswordResetMutation,
-    usePostValidateTokenMutation,
-    usePostResetPasswordMutation,
-    useGetUserInfoQuery,
-    useGetUserInfoAdditionalQuery,
-    usePutUpdateUserInfoMutation,
-    usePostSendResetPasswordMutation,
-    usePostUploadLogoMutation
-} = userApiSlice
+  usePostCheckEmailMutation,
+  usePostRegisterUserMutation,
+  useGetVerifyUserQuery,
+  usePostRequestPasswordResetMutation,
+  usePostValidateTokenMutation,
+  usePostResetPasswordMutation,
+  useGetUserInfoQuery,
+  useGetUserInfoAdditionalQuery,
+  usePutUpdateUserInfoMutation,
+  usePostSendResetPasswordMutation,
+  usePostUploadLogoMutation,
+  useSaveResultMutation,
+} = userApiSlice;
