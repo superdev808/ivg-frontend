d5576adcab52b504e6ae54ba8eb4fbf2e6e8e750
alok.mali <alok.mali@galaxyweblinks.in>
2024-02-15 19:43:24 +0530

    updated styles

--- src/components/secure/calculator/AllOnX/PdfExport/PatientInfo/PatientInfo.tsx
+++ src/components/secure/calculator/AllOnX/PdfExport/PatientInfo/PatientInfo.tsx
@@ -16,13 +16,13 @@ interface FormValues {
   "Office Staff": string[];
   Patient: string[];
 }
-interface Recipient { 
-  name: string, 
-  key: string, 
-  hasInput: boolean 
+interface Recipient {
+  name: string;
+  key: string;
+  hasInput: boolean;
 }
 interface RecipientEmail {
-  [key: string]: string[]
+  [key: string]: string[];
 }
 interface PatientInfoProps {
   onSubmit: (data: FormValues) => void;
@@ -35,9 +35,9 @@ const PatientInfo: React.FC<PatientInfoProps> = ({ info, onSubmit }) => {
     address: "",
     filename: info?.filename || "",
     recipientsList: "",
-    Dentist:[],
-    "Office Staff":[],
-    Patient:[],
+    Dentist: [],
+    "Office Staff": [],
+    Patient: [],
   };
   const {
     register,
@@ -56,54 +56,56 @@ const PatientInfo: React.FC<PatientInfoProps> = ({ info, onSubmit }) => {
     Object.values(recipientEmails).map((emails: string[]) => {
       emails.map((email: string) => _recipientsList.push(email));
     });
-    const emails = _recipientsList.join('|');
+    const emails = _recipientsList.join("|");
     setValue(`recipientsList`, emails);
-    if(!!emails){
-      clearErrors('recipientsList');
+    if (!!emails) {
+      clearErrors("recipientsList");
     }
-  },[recipientEmails, setValue])
+  }, [recipientEmails, setValue]);
 
   const recipients: Recipient[] = [
-    { name: 'Myself', key: 'Myself', hasInput: false },
-    { name: 'Dentist', key: 'Dentist', hasInput: true },
-    { name: 'Office Staff', key: 'Office Staff', hasInput: true },
-    { name: 'Patient', key: 'Patient', hasInput: true }
+    { name: "Myself", key: "Myself", hasInput: false },
+    { name: "Dentist", key: "Dentist", hasInput: true },
+    { name: "Office Staff", key: "Office Staff", hasInput: true },
+    { name: "Patient", key: "Patient", hasInput: true },
   ];
 
   const onRecipientsChange = (e: CheckboxChangeEvent) => {
-      let _selectedRecipients: Recipient[] = [...selectedRecipients];
-      const recipient: Recipient = e.value;
-      if (e.checked){
-        _selectedRecipients.push(recipient);
-        if(recipient.key === "Myself"){
-          const loggedInUserEmail = getCookie("email") || "";
-          handleRecipientEmailChange([loggedInUserEmail], "Myself")
-        }
-      }    
-      else {
-        _selectedRecipients = _selectedRecipients.filter(item => item.key !== recipient.key);
-        const _recipientEmails = {...recipientEmails};
-        delete _recipientEmails[recipient.key]
-        setValue(`${recipient.key}` as any, []);
-        setRecipientEmails(_recipientEmails);
+    let _selectedRecipients: Recipient[] = [...selectedRecipients];
+    const recipient: Recipient = e.value;
+    if (e.checked) {
+      _selectedRecipients.push(recipient);
+      if (recipient.key === "Myself") {
+        const loggedInUserEmail = getCookie("email") || "";
+        handleRecipientEmailChange([loggedInUserEmail], "Myself");
       }
-      setSelectedRecipients(_selectedRecipients);    
+    } else {
+      _selectedRecipients = _selectedRecipients.filter(
+        (item) => item.key !== recipient.key
+      );
+      const _recipientEmails = { ...recipientEmails };
+      delete _recipientEmails[recipient.key];
+      setValue(`${recipient.key}` as any, []);
+      setRecipientEmails(_recipientEmails);
+    }
+    setSelectedRecipients(_selectedRecipients);
   };
 
-  const handleRecipientEmailChange = (emails: string[], recipientType: string) => {
-    let _recipientEmails: RecipientEmail = {...recipientEmails};
-    const newEmails: RecipientEmail = {[recipientType]: emails}
-    _recipientEmails = {..._recipientEmails, ...newEmails}
+  const handleRecipientEmailChange = (
+    emails: string[],
+    recipientType: string
+  ) => {
+    let _recipientEmails: RecipientEmail = { ...recipientEmails };
+    const newEmails: RecipientEmail = { [recipientType]: emails };
+    _recipientEmails = { ..._recipientEmails, ...newEmails };
     setRecipientEmails(_recipientEmails);
-  }
+  };
 
   const getFormErrorMessage = (name: string) => {
     type errors = { [key: string]: { message: string } };
     return (errors as errors)[name] ? (
       <small className="p-error">{(errors as errors)[name].message}</small>
-    ) : (
-      null
-    );
+    ) : null;
   };
 
   return (
@@ -183,71 +185,83 @@ const PatientInfo: React.FC<PatientInfoProps> = ({ info, onSubmit }) => {
         )}
       </div>
 
-      {info?.actionType === "export" && <div className="mb-3">
-        <p>Who are you sending this to?</p>
-        <div className="flex flex-column gap-3">
-          {recipients.map((recipient: Recipient) => {
-            const isChecked: boolean = selectedRecipients.some(
-              (item: Recipient) => item.key === recipient.key
-            );
-            return (
-              <>
-                <div key={recipient.key} className="flex align-items-center">
-                  <Checkbox
-                    inputId={recipient.key}
-                    name="recipient"
-                    value={recipient}
-                    onChange={onRecipientsChange}
-                    checked={isChecked}
-                  />
-                  <label htmlFor={recipient.key} className="ml-2">
-                    {recipient.name}
-                  </label>
-                </div>
-                {recipient.hasInput && isChecked && (
-                  <>                    
-                    <Controller
-                      name={`${recipient.key}` as any}
-                      control={control}
-                      rules={{ required: "Please enter email address.", validate: (value: string[]) => value.every((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) || 'Please enter valid email addresses'}}
-                      render={({ field, fieldState }) => (
-                        <Chips
-                          addOnBlur={true}
-                          separator=","
-                          id={field.name}
-                          name={recipient.key}
-                          value={field.value}
-                          onChange={(e) => {
-                            field.onChange(e.value);
-                            handleRecipientEmailChange(
-                              e.value || [],
-                              recipient.key
-                            );
-                          }}
-                          className={fieldState.error ? "p-invalid" : ""}
-                        />
-                      )}
+      {info?.actionType === "export" && (
+        <div className="mb-3">
+          <p>Who are you sending this to?</p>
+          <div className="flex flex-column">
+            {recipients.map((recipient: Recipient) => {
+              const isChecked: boolean = selectedRecipients.some(
+                (item: Recipient) => item.key === recipient.key
+              );
+              return (
+                <>
+                  <div key={recipient.key} className="flex align-items-center pb-3">
+                    <Checkbox
+                      inputId={recipient.key}
+                      name="recipient"
+                      value={recipient}
+                      onChange={onRecipientsChange}
+                      checked={isChecked}
                     />
-                    {getFormErrorMessage(recipient.key)}
-                  </>
-                )}
-              </>
-            );
-          })}
-        </div>  
-        <InputText
-          hidden
-          id="recipientsList"
-          {...register("recipientsList", {
-            required: true,
-            validate: (value) => !!value.trim(),
-          })}
-          className={errors.recipientsList ? "p-invalid" : ""}
-        />
-        {errors.recipientsList && (
-          <small className="p-error">Please provide atleast one recipient.</small>
-        )}     
-      </div>}
+                    <label htmlFor={recipient.key} className="ml-2">
+                      {recipient.name}
+                    </label>
+                  </div>
+                  {recipient.hasInput && isChecked && (
+                    <div className="mb-3">
+                      <Controller
+                        name={`${recipient.key}` as any}
+                        control={control}
+                        rules={{
+                          required: "Please enter email address.",
+                          validate: (value: string[]) =>
+                            value.every((email) =>
+                              /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+                            ) || "Please enter valid email addresses",
+                        }}
+                        render={({ field, fieldState }) => (
+                          <>
+                            <Chips
+                              addOnBlur={true}
+                              separator=","
+                              id={field.name}
+                              name={recipient.key}
+                              value={field.value}
+                              onChange={(e) => {
+                                field.onChange(e.value);
+                                handleRecipientEmailChange(
+                                  e.value || [],
+                                  recipient.key
+                                );
+                              }}
+                              className={fieldState.error ? "p-invalid" : ""}
+                            />
+                            {getFormErrorMessage(recipient.key)}
+                          </>
+                        )}
+                      />
+                    </div>
+                  )}
+                </>
+              );
+            })}
+          </div>
+          <InputText
+            hidden
+            id="recipientsList"
+            {...register("recipientsList", {
+              required: true,
+              validate: (value) => !!value.trim(),
+            })}
+            className={errors.recipientsList ? "p-invalid" : ""}
+          />
+          {errors.recipientsList && (
+            <small className="p-error">
+              Please provide atleast one recipient.
+            </small>
+          )}
+        </div>
+      )}
 
       <Button type="submit" label="Submit" />
     </form>
