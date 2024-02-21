1f4748e254293f71a58a00b30a128464985dbcce
Wayne Kuo <waynekuo1123@gmail.com>
2024-02-21 10:03:37 -0800

    update all-on-x name

--- src/app/(protected)/calculators/constants.ts
+++ src/app/(protected)/calculators/constants.ts
@@ -1,4 +1,4 @@
 export const CALCULATOR_MAPPINGS: { [key: string]: string } = {
-  ALL_ON_X_CALCULATOR: "All-on-X Surgery and Restorative",
+  ALL_ON_X_CALCULATOR: "All-on-X Ordering Guide",
   CUSTOM_COMBINATION: "Custom Combinations",
 };
